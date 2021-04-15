ffeeafaa062af5a0276beee95ef611d0c080b4ad
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-15 18:51:54 +0300

    launches and recordes a video

--- LaunchService/Worker.cs
+++ LaunchService/Worker.cs
@@ -27,36 +27,94 @@ namespace LaunchService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            // launch the meeting
+
             await Task.Delay(4 * 1000, stoppingToken);
             _logger.LogInformation("Starting the scheduled meeeting...");
             await Task.Delay(1 * 1000, stoppingToken);
             _logger.LogInformation("Launching link in the browser...");
 
-            _launcher.Launch("https://us04web.zoom.us/j/74772721215?pwd=U0t0b1JvSDZZZVZ4alh2RHZXNXYzZz09");
+            string ZOOM_MEETING_LINK = "https://us04web.zoom.us/j/71470382288?pwd=UWdTZFdWRkR4bE11M1JMVkJqY2ZlUT09";
+            string basePath = "C:\\DISKD\\";
+            string videoName = "meeting.mp4";
+
+            _launcher.Launch(ZOOM_MEETING_LINK);
 
             _logger.LogInformation("Meeting launched successfully!");
 
-            // record video
+            // record the video
 
             _logger.LogInformation("Starting the recording of the meeeting...");
             await Task.Delay(3 * 1000, stoppingToken);
-            CreateRecording();
+            CreateRecording(basePath, videoName); 
             _logger.LogInformation("Meeting recording successfully started!");
-            await Task.Delay(60 * 1000, stoppingToken);
+            await Task.Delay(30 * 1000, stoppingToken);
             EndRecording();
             _logger.LogInformation("Meeting recording successfully finished!");
         }
 
         /// <summary>
-        /// Recorder
+        /// Video Recorder
         /// </summary>
 
         Recorder _rec;
         Stream _outStream;
-        void CreateRecording()
+        void CreateRecording(string basePath, string videoName)
         {
-            string videoPath = Path.Combine("C:\\DISKD\\", "test.mp4");
-            _rec = Recorder.CreateRecorder();
+            string videoPath = Path.Combine(basePath, videoName);
+
+            var inputDevices = Recorder.GetSystemAudioDevices(AudioDeviceSource.InputDevices);
+            var outputDevices = Recorder.GetSystemAudioDevices(AudioDeviceSource.OutputDevices);
+            string selectedOutputDevice = null;
+            string selectedInputDevice = null;
+
+            RecorderOptions options = new RecorderOptions
+            {
+                RecorderMode = RecorderMode.Video,
+                //If throttling is disabled, out of memory exceptions may eventually crash the program,
+                //depending on encoder settings and system specifications.
+                IsThrottlingDisabled = false,
+                //Hardware encoding is enabled by default.
+                IsHardwareEncodingEnabled = true,
+                //Low latency mode provides faster encoding, but can reduce quality.
+                IsLowLatencyEnabled = false,
+                //Fast start writes the mp4 header at the beginning of the file, to facilitate streaming.
+                IsMp4FastStartEnabled = false,
+                AudioOptions = new AudioOptions
+                {
+                    Bitrate = AudioBitrate.bitrate_128kbps,
+                    Channels = AudioChannels.Stereo,
+                    IsAudioEnabled = true,
+                    IsOutputDeviceEnabled = true,
+                    IsInputDeviceEnabled = true,
+                    AudioOutputDevice = selectedOutputDevice,
+                    AudioInputDevice = selectedInputDevice
+                },
+                VideoOptions = new VideoOptions
+                {
+                    BitrateMode = BitrateControlMode.UnconstrainedVBR,
+                    Bitrate = 8000 * 1000,
+                    Framerate = 60,
+                    IsFixedFramerate = true,
+                    EncoderProfile = H264Profile.Main
+                },
+                MouseOptions = new MouseOptions
+                {
+                    //Displays a colored dot under the mouse cursor when the left mouse button is pressed.	
+                    IsMouseClicksDetected = true,
+                    MouseClickDetectionColor = "#FFFF00",
+                    MouseRightClickDetectionColor = "#FFFF00",
+                    MouseClickDetectionRadius = 30,
+                    MouseClickDetectionDuration = 100,
+
+                    IsMousePointerEnabled = true,
+                    /* Polling checks every millisecond if a mouse button is pressed.
+                       Hook works better with programmatically generated mouse clicks, but may affect
+                       mouse performance and interferes with debugging.*/
+                    MouseClickDetectionMode = MouseDetectionMode.Hook
+                }
+            };
+            _rec = Recorder.CreateRecorder(options);
             _rec.OnRecordingComplete += Rec_OnRecordingComplete;
             _rec.OnRecordingFailed += Rec_OnRecordingFailed;
             _rec.OnStatusChanged += Rec_OnStatusChanged;
