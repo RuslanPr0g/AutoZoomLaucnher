9cef959cbbc2f84ca62bab1ebebc3e5d95798a0f
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-15 16:56:53 +0300

    remove useless loop

--- LaunchService/Worker.cs
+++ LaunchService/Worker.cs
@@ -19,11 +19,7 @@ namespace LaunchService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+
         }
     }
 }
