0087d926926fe43363751913c91ec4c3d727332a
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-16 09:03:21 +0300

    add recording model and apply srp for recorder

--- LaunchService/Program.cs
+++ LaunchService/Program.cs
@@ -1,4 +1,7 @@
+using Hangfire;
+using Hangfire.MemoryStorage;
 using LaunchService.Launcher;
+using LaunchService.Recorders;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -22,6 +25,15 @@ namespace LaunchService
                     services.AddHostedService<Worker>();
 
                     services.AddTransient<IMeetingLauncher, MeetingLauncher>();
+                    services.AddTransient<ScreenRecorder>();
+
+                    services.AddHangfire(config =>
+                        config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                        .UseSimpleAssemblyNameTypeSerializer()
+                        .UseDefaultTypeSerializer()
+                        .UseMemoryStorage());
+
+                    services.AddHangfireServer();
                 });
     }
 }
--- LaunchService/Worker.cs
+++ LaunchService/Worker.cs
@@ -1,13 +1,9 @@
 using LaunchService.Launcher;
+using LaunchService.Models;
+using LaunchService.Recorders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Win32;
-using ScreenRecorderLib;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,150 +13,57 @@ namespace LaunchService
     {
         private readonly ILogger<Worker> _logger;
         private readonly IMeetingLauncher _launcher;
+        private readonly ScreenRecorder _screenRecorder;
 
         public Worker(ILogger<Worker> logger,
-            IMeetingLauncher launcher)
+            IMeetingLauncher launcher, ScreenRecorder screenRecorder)
         {
             _logger = logger;
             _launcher = launcher;
+            _screenRecorder = screenRecorder;
         }
-
-        /// <summary>
-        /// TODO:
-        /// 1) Add scheduling mechanism using cron.
-        /// 2) After 41 minutes - launch meeting the second time do not stop recording
-        /// </summary>
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            // launch the meeting
+            // configure the jobs
+
+            // ...
+
+
+
+
+            // launching the meeting
 
             await Task.Delay(2 * 1000, stoppingToken);
 
             Console.WriteLine("Enter a zoom link: ");
             string ZOOM_MEETING_LINK = Console.ReadLine();
 
-            Console.WriteLine("Enter video name (without an extention): ");
-            string videoName = Console.ReadLine() + ".mp4";
+            Console.WriteLine("Enter meeting name (without an extention): ");
+            string meetingName = DateTime.Now.Date.Year + "-" + DateTime.Now.Date.Month + "-" 
+                + DateTime.Now.Date.Day + "_" + Console.ReadLine() + ".mp4";
 
             Console.WriteLine("Enter duration of the video (in seconds): ");
             int secondsToRecord = Convert.ToInt32(Console.ReadLine());
+
+            const string basePath = "C:\\DISKD\\";
 
             _logger.LogInformation("Starting the scheduled meeeting...");
             await Task.Delay(1 * 1000, stoppingToken);
             _logger.LogInformation("Launching link in the browser...");
 
-            //string ZOOM_MEETING_LINK = "https://us04web.zoom.us/j/71470382288?pwd=UWdTZFdWRkR4bE11M1JMVkJqY2ZlUT09";
-            string basePath = "C:\\DISKD\\";
-            //string videoName = "meeting.mp4";
-
             _launcher.Launch(ZOOM_MEETING_LINK);
-
             _logger.LogInformation("Meeting launched successfully!");
 
-            // record the video
+            // recording the video
 
             _logger.LogInformation("Starting the recording of the meeeting...");
             await Task.Delay(3 * 1000, stoppingToken);
-            CreateRecording(basePath, videoName);
+            _screenRecorder.CreateRecording(basePath, meetingName);
             _logger.LogInformation("Meeting recording successfully started!");
             await Task.Delay(secondsToRecord * 1000, stoppingToken);
-            EndRecording();
+            _screenRecorder.EndRecording();
             _logger.LogInformation("Meeting recording successfully finished!");
-        }
-
-        /// <summary>
-        /// Video Recorder
-        /// </summary>
-
-        Recorder _rec;
-        Stream _outStream;
-        void CreateRecording(string basePath, string videoName)
-        {
-            string videoPath = Path.Combine(basePath, videoName);
-
-            var inputDevices = Recorder.GetSystemAudioDevices(AudioDeviceSource.InputDevices);
-            var outputDevices = Recorder.GetSystemAudioDevices(AudioDeviceSource.OutputDevices);
-            string selectedOutputDevice = null;
-            string selectedInputDevice = null;
-
-            RecorderOptions options = new RecorderOptions
-            {
-                RecorderMode = RecorderMode.Video,
-                //If throttling is disabled, out of memory exceptions may eventually crash the program,
-                //depending on encoder settings and system specifications.
-                IsThrottlingDisabled = false,
-                //Hardware encoding is enabled by default.
-                IsHardwareEncodingEnabled = true,
-                //Low latency mode provides faster encoding, but can reduce quality.
-                IsLowLatencyEnabled = false,
-                //Fast start writes the mp4 header at the beginning of the file, to facilitate streaming.
-                IsMp4FastStartEnabled = false,
-                AudioOptions = new AudioOptions
-                {
-                    Bitrate = AudioBitrate.bitrate_128kbps,
-                    Channels = AudioChannels.Stereo,
-                    IsAudioEnabled = true,
-                    IsOutputDeviceEnabled = true,
-                    IsInputDeviceEnabled = true,
-                    AudioOutputDevice = selectedOutputDevice,
-                    AudioInputDevice = selectedInputDevice
-                },
-                VideoOptions = new VideoOptions
-                {
-                    BitrateMode = BitrateControlMode.UnconstrainedVBR,
-                    Bitrate = 8000 * 1000,
-                    Framerate = 60,
-                    IsFixedFramerate = true,
-                    EncoderProfile = H264Profile.Main
-                },
-                MouseOptions = new MouseOptions
-                {
-                    //Displays a colored dot under the mouse cursor when the left mouse button is pressed.	
-                    IsMouseClicksDetected = true,
-                    MouseClickDetectionColor = "#FFFF00",
-                    MouseRightClickDetectionColor = "#FFFF00",
-                    MouseClickDetectionRadius = 30,
-                    MouseClickDetectionDuration = 100,
-
-                    IsMousePointerEnabled = true,
-                    /* Polling checks every millisecond if a mouse button is pressed.
-                       Hook works better with programmatically generated mouse clicks, but may affect
-                       mouse performance and interferes with debugging.*/
-                    MouseClickDetectionMode = MouseDetectionMode.Hook
-                }
-            };
-            _rec = Recorder.CreateRecorder(options);
-            _rec.OnRecordingComplete += Rec_OnRecordingComplete;
-            _rec.OnRecordingFailed += Rec_OnRecordingFailed;
-            _rec.OnStatusChanged += Rec_OnStatusChanged;
-            //Record to a file
-            //videoPath = Path.Combine(Path.GetTempPath(), "test.mp4");
-            _rec.Record(videoPath);
-            //..Or to a stream
-            //_outStream = new MemoryStream();
-            //_rec.Record(_outStream);
-        }
-        void EndRecording()
-        {
-            _rec.Stop();
-        }
-        private void Rec_OnRecordingComplete(object sender, RecordingCompleteEventArgs e)
-        {
-            //Get the file path if recorded to a file
-            string path = e.FilePath;
-            //or do something with your stream
-            //... something ...
-            _outStream?.Dispose();
-        }
-        private void Rec_OnRecordingFailed(object sender, RecordingFailedEventArgs e)
-        {
-            string error = e.Error;
-            _outStream?.Dispose();
-        }
-        private void Rec_OnStatusChanged(object sender, RecordingStatusEventArgs e)
-        {
-            RecorderStatus status = e.Status;
         }
     }
 }
